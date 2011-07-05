978f49797825281949987f4960be7120c87daf59
Stephan <ssoileau@gmail.com>
2011-07-05 11:04:03 -0700

    Fix an amazingly cool bug. Since you have no idea where the closure will get executed, the function must be fully qualified to prevent from exploding.

--- src/machinist/behat/steps/machinist_steps.php
+++ src/machinist/behat/steps/machinist_steps.php
@@ -1,5 +1,4 @@
 <?php
-use \machinist\behat\functions\createMachinesFromTable;
 $steps->Given('/^the following (\w+) exists:$/', function($world, $blueprint, $table) {
-	createMachinesFromTable($world, $blueprint, $table);
+	\machinist\behat\functions\createMachinesFromTable($world, $blueprint, $table);
 });
