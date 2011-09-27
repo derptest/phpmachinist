7965371ee2e5222af83c50b80d63371e86a2e80e
Scott Scoble <punkdgeek@gmail.com>
2011-09-27 14:15:26 -0700

    Fixed spelling error Give to Given

--- src/machinist/behat/steps/machinist_steps.php
+++ src/machinist/behat/steps/machinist_steps.php
@@ -2,7 +2,7 @@
 $steps->Given('/^the following (\w+) exists:$/', function($world, $blueprint, $table) {
 	\machinist\behat\functions\createMachinesFromTable($world, $blueprint, $table);
 });
-$steps->Give('/^there are no (\w+) machines$/', function($world, $bp) {
+$steps->Given('/^there are no (\w+) machines$/', function($world, $bp) {
 	\machinist\Machinist::wipe($bp, true);
 });
 $steps->Given('/^there are no machines$/', function($world) {
