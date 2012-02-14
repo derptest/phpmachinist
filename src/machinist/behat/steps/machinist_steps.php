2bfb9b2dbf0b7fa3a4c1b27e62f2ccde15b3426f
Adam Englander <adam.englander@dataxltd.com>
2012-02-13 19:00:28 -0800

    Fix wipe all Behat step

--- src/machinist/behat/steps/machinist_steps.php
+++ src/machinist/behat/steps/machinist_steps.php
@@ -6,5 +6,5 @@ $steps->Given('/^there are no (\w+) machines$/', function($world, $bp) {
 	\machinist\Machinist::wipe($bp, true);
 });
 $steps->Given('/^there are no machines$/', function($world) {
-	\machinist\Machinist::wipeAll(true);
+	\machinist\Machinist::instance()->wipeAll(true);
 });
